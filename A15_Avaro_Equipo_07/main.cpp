53e3ff9f69d6d3009b9421b909027aef87e803ec
Emiliano <a01710711@tec.mx>
2024-08-28 14:24:07 -0600

    Add greddy algorithm activity template

---
+++ A15_Avaro_Equipo_07/main.cpp
@@ -0,0 +1,26 @@
+// -----------------------------------------------------
+
+// Title: Implementation of the "Greedy Algorithms" programming technique
+
+// Created On: 28/08/2024
+// Updated On: 28/08/2024
+
+// Authors:
+//     Angel Mauricio Ramirez Herrera - A01710158
+//     Cristian Chavez Guia - A0171680
+//     Emiliano Gomez Gonzalez - A01710711
+
+// Code Description:
+//
+//
+//
+//
+//
+
+// -----------------------------------------------------
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
