33f9e83ce27026f540248493003f8625fe3fb0b3
Emiliano <a01710711@tec.mx>
2024-11-10 19:13:35 -0600

    Prim

---
+++ ActInt2_Equipo_07/prim.py
@@ -0,0 +1,44 @@
+import sys
+
+def prim_mst(graph, n):
+    selected_node = [False] * n
+    selected_node[0] = True
+    mst_edges = []
+    total_cost = 0
+
+    for _ in range(n - 1):
+        min_weight = sys.maxsize
+        a = b = -1
+
+        for u in range(n):
+            if selected_node[u]:
+                for v in range(n):
+                    if not selected_node[v] and graph[u][v] > 0:
+                        if graph[u][v] < min_weight:
+                            min_weight = graph[u][v]
+                            a, b = u, v
+
+        if a != -1 and b != -1:
+            mst_edges.append((a, b))
+            total_cost += min_weight
+            print(total_cost)
+            selected_node[b] = True
+
+    return mst_edges, total_cost
+
+# lectura de la matriz de adyacencias desde un archivo
+def leer_matriz_adyacencia(nombre_archivo):
+    with open(nombre_archivo, 'r') as f:
+        n = int(f.readline().strip())
+        graph = []
+        for _ in range(n):
+            fila = list(map(int, f.readline().strip().split()))
+            graph.append(fila)
+    return n, graph
+
+# uso
+nombre_archivo = "input.txt"
+n, graph = leer_matriz_adyacencia(nombre_archivo)
+mst_edges, total_cost = prim_mst(graph, n)
+print("Aristas del MST:", mst_edges)
+print("Costo total de fibra óptica:", total_cost)
